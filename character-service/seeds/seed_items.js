7821e12aa5580fae5c6d2c080ae6c198cfe797d6
stefan <stefann.stojanovic@gmail.com>
2025-03-14 19:26:06 +0100

    Add Postman collections and environments

--- character-service/seeds/seed_items.js
+++ character-service/seeds/seed_items.js
@@ -5,22 +5,30 @@ exports.seed = async function (knex) {
     {
       name: "Sword",
       description: "A mighty sword that grants strength",
-      bonus_strength: 5,
+      bonus_strength: 8,
+    },
+    {
+      name: "Dagger",
+      description: "A dagger that enhances agility and intelligence",
+      bonus_agility: 7,
+      bonus_agility: 5,
     },
     {
       name: "Boots",
       description: "Boots that increase agility",
-      bonus_agility: 3,
+      bonus_agility: 4,
     },
     {
       name: "Staff",
       description: "A staff that boosts intelligence",
-      bonus_intelligence: 4,
+      bonus_intelligence: 6,
     },
     {
       name: "Ring",
-      description: "A ring that enhances faith",
-      bonus_faith: 2,
+      description: "A ring that enhances strength, intelligence and faith",
+      bonus_strength: 3,
+      bonus_intelligence: 3,
+      bonus_faith: 3,
     },
   ]);
 };
